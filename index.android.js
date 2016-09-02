c4900de798c284a225f1bfdd38a9130c17a3bd58
Franze Jr <franzejr@gmail.com>
2016-09-01 22:40:25 -0300

    Add TabBarIOS for iOS version

--- index.android.js
+++ index.android.js
@@ -1,16 +1,29 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
 } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
 
 class platformSpecificComponents extends Component {
   render() {
@@ -31,23 +44,5 @@ class platformSpecificComponents extends Component {
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('platformSpecificComponents', () => platformSpecificComponents);
--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,68 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  TabBarIOS,
+  View,
 } from 'react-native';
 
-class platformSpecificComponents extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
-
 const styles = StyleSheet.create({
-  container: {
+  container1: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#414A8C',
+  },
+  container2: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
 });
+
+class platformSpecificComponents extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { selectedTab: 'tab1' };
+  }
+  render() {
+    return (
+      <TabBarIOS
+        unselectedTintColor="yellow"
+        tintColor="white"
+        barTintColor="darkslateblue"
+      >
+        <TabBarIOS.Item
+          title="Tab 1"
+          onPress={() => {
+            this.setState({
+              selectedTab: 'tab1',
+            }); }}
+          selected={this.state.selectedTab === 'tab1'}
+        >
+          <View style={styles.container1}>
+            <Text>Tab 1</Text>
+          </View>
+        </TabBarIOS.Item>
+        <TabBarIOS.Item
+          onPress={() => {
+            this.setState({
+              selectedTab: 'tab2',
+            }); }}
+          selected={this.state.selectedTab === 'tab2'}
+          renderAsOriginal
+          title="Tab 2"
+        >
+          <View style={styles.container2}>
+            <Text>Tab 2</Text>
+          </View>
+        </TabBarIOS.Item>
+      </TabBarIOS>
+    );
+  }
+}
+
 
 AppRegistry.registerComponent('platformSpecificComponents', () => platformSpecificComponents);
